57458a48399cdcce2939f35236a0442a72a540b2
Fitti <dev@fitti.io>
2026-07-28 12:59:29 +0200

    Test some file reading and writing

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,9 @@ const fatal = process.fatal;
 const mem = std.mem;
 
 const study = @import("study");
+const Lecture = study.Lecture;
+
+const PriorityQueue = std.PriorityQueue(Lecture, void, Lecture.compareFn);
 
 pub fn main(init: process.Init) !void {
     const io = init.io;
@@ -23,27 +26,7 @@ pub fn main(init: process.Init) !void {
     std.debug.print("Command: {t}\n", .{cmd});
     if (name) |str| std.debug.print("Lecture name: {s}\n", .{str});
 
-    // Handle the command
-    switch (cmd) {
-        .help => try stdout.print("{s}\n", .{help_text}),
-        .queue, .pop, .new => {},
-    }
-    try stdout.flush();
-
-    const base_data_dir_path = dataHome(arena, init.environ_map) catch |err| switch (err) {
-        error.HomeNotSet => fatal("Could not find home directory, $HOME not set.", .{}),
-        else => return err,
-    };
-    const base_data_dir = try std.Io.Dir.openDirAbsolute(io, base_data_dir_path, .{});
-    defer base_data_dir.close(io);
-
-    const data_dir = if (base_data_dir.openDir(io, "study", .{})) |dir| dir else |err| blk: switch (err) {
-        error.FileNotFound => {
-            try base_data_dir.createDir(io, "study", .default_dir);
-            break :blk try base_data_dir.openDir(io, "study", .{});
-        },
-        else => return err,
-    };
+    const data_dir = try dataDir(arena, io, init.environ_map);
     defer data_dir.close(io);
 
     var atomic_file_current = try data_dir.createFileAtomic(io, "current.txt", .{ .replace = true });
@@ -53,10 +36,70 @@ pub fn main(init: process.Init) !void {
     var writer_current: std.Io.File.Writer = .init(atomic_file_current.file, io, &buf_file_current);
     const current = &writer_current.interface;
 
-    try current.writeAll("Hello from study!");
+    var current_queue = try getQueue(arena, io, data_dir, "current.txt");
+    while (current_queue.pop()) |lecture| {
+        try stdout.print("{f}\n", .{std.fmt.Alt(Lecture, Lecture.formatHuman){ .data = lecture }});
+        try current.print("{f}\n", .{lecture});
+    }
+    try stdout.flush();
     try current.flush();
 
     try atomic_file_current.replace(io);
+
+    var atomic_file_next = try data_dir.createFileAtomic(io, "next.txt", .{ .replace = true });
+    defer atomic_file_next.deinit(io);
+
+    var buf_file_next: [1024]u8 = undefined;
+    var writer_next: std.Io.File.Writer = .init(atomic_file_next.file, io, &buf_file_next);
+    const next = &writer_next.interface;
+
+    var next_queue = try getQueue(arena, io, data_dir, "next.txt");
+    while (next_queue.pop()) |lecture| {
+        try stdout.print("{f}\n", .{std.fmt.Alt(Lecture, Lecture.formatHuman){ .data = lecture }});
+        try next.print("{f}\n", .{lecture});
+    }
+    try stdout.flush();
+    try next.flush();
+
+    try atomic_file_next.replace(io);
+
+    // Handle the command
+    switch (cmd) {
+        .help => try stdout.print("{s}\n", .{help_text}),
+        .queue, .pop, .new => {},
+    }
+    try stdout.flush();
+}
+
+fn getQueue(gpa: mem.Allocator, io: std.Io, dir: std.Io.Dir, filename: []const u8) !PriorityQueue {
+    const raw = dir.readFileAlloc(io, filename, gpa, .unlimited) catch |err| switch (err) {
+        error.FileNotFound => null,
+        else => return err,
+    };
+    var queue: PriorityQueue = .empty;
+    if (raw) |str| {
+        var iter = std.mem.tokenizeScalar(u8, str, '\n');
+        while (iter.next()) |line| try queue.push(gpa, try .fromString(line));
+    }
+
+    return queue;
+}
+
+fn dataDir(gpa: mem.Allocator, io: std.Io, env: *const process.Environ.Map) !std.Io.Dir {
+    const base_data_dir_path = dataHome(gpa, env) catch |err| switch (err) {
+        error.HomeNotSet => fatal("Could not find home directory, $HOME not set.", .{}),
+        else => return err,
+    };
+    const base_data_dir = try std.Io.Dir.openDirAbsolute(io, base_data_dir_path, .{});
+    defer base_data_dir.close(io);
+
+    return if (base_data_dir.openDir(io, "study", .{})) |dir| dir else |err| blk: switch (err) {
+        error.FileNotFound => {
+            try base_data_dir.createDir(io, "study", .default_dir);
+            break :blk try base_data_dir.openDir(io, "study", .{});
+        },
+        else => return err,
+    };
 }
 
 fn dataHome(gpa: mem.Allocator, env: *const process.Environ.Map) ![]const u8 {
