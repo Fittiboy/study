bcd2f9b572e034cf4c564290d7202f6f59336140
Fitti <dev@fitti.io>
2026-08-09 07:43:28 +0200

    Create build step for multiple release targets

--- build.zig
+++ build.zig
@@ -1,24 +1,41 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+const exe_name = "study";
+const release_targets: []const std.Target.Query = &.{
+    // macOS
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+
+    // Linux
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+
+    // Windows
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.createModule(.{
+    const root_mod = b.createModule(.{
         .target = target,
         .optimize = optimize,
         .root_source_file = b.path("src/root.zig"),
     });
 
+    const root_source_file_exe: std.Build.LazyPath = b.path("src/main.zig");
+    try release(b, root_source_file_exe, root_mod);
+
     const exe = b.addExecutable(.{
-        .name = "study",
+        .name = exe_name,
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = root_source_file_exe,
         }),
     });
-    exe.root_module.addImport("study", mod);
+    exe.root_module.addImport("study", root_mod);
 
     //  ___           _        _ _
     // |_ _|_ __  ___| |_ __ _| | |
@@ -47,7 +64,7 @@ pub fn build(b: *std.Build) void {
     //
     const test_step = b.step("test", "Run the tests");
     const test_exe = b.addRunArtifact(b.addTest(.{
-        .root_module = mod,
+        .root_module = root_mod,
     }));
     test_step.dependOn(&test_exe.step);
 
@@ -59,4 +76,39 @@ pub fn build(b: *std.Build) void {
     //                               \_\       /_/
     const check = b.step("check", "The check step for zls build-on-save");
     check.dependOn(&exe.step);
+}
+
+pub fn release(
+    b: *std.Build,
+    root_exe: std.Build.LazyPath,
+    root_mod: *std.Build.Module,
+) !void {
+    const release_step = b.step(
+        "release",
+        "Build release binaries for preconfigured targets",
+    );
+
+    for (release_targets) |target| {
+        const exe = b.addExecutable(.{
+            .name = exe_name,
+            .root_module = b.createModule(.{
+                .root_source_file = root_exe,
+                .target = b.resolveTargetQuery(target),
+                .optimize = .ReleaseSafe,
+                .imports = &.{
+                    .{ .name = "study", .module = root_mod },
+                },
+            }),
+        });
+
+        const target_output = b.addInstallArtifact(exe, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try target.zigTriple(b.allocator),
+                },
+            },
+        });
+
+        release_step.dependOn(&target_output.step);
+    }
 }
